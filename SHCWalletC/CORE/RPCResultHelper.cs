aa644cbc8b3f1faf85ce25a98f40d5dea83cfaf4
JohnJohnssonnl <JohnJohnssonnl@gmail.com>
2018-05-09 19:40:08 +0200

    Avoid stacktraces

--- SHCWalletC/CORE/RPCResultHelper.cs
+++ SHCWalletC/CORE/RPCResultHelper.cs
@@ -11,10 +11,19 @@ namespace SHCWalletC.CORE
         public static string[] jsonResult(GenericResponse _response)
         {
             IEnumerator resultsEnumerator;
-            IEnumerable results = _response.Result.Values();
-            resultsEnumerator = results.GetEnumerator();
+            IEnumerable results;
+
             string[] values = new string[0];
             int x = 0;
+
+            if (_response == null)
+            {
+                //Avoid errors when connection did not result in a response
+                return values;  //Return empty
+            }
+
+            results = _response.Result.Values();
+            resultsEnumerator = results.GetEnumerator();
 
             while (resultsEnumerator.MoveNext())
             {
